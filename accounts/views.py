ec5e7d0bda432072904cae4f759eacb35d3165bf
adulam <adulam3@gatech.edu>
2025-02-03 23:15:10 -0500

    added the signup link to the base template

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
-# from .forms import CustomUserCreationForm, CustomErrorList
-from django.shortcuts import redirect
-from django.contrib.auth.decorators import login_required
-
-# Create your views here.
 from django.contrib.auth.forms import UserCreationForm
 def signup(request):
     template_data = {}
@@ -13,26 +7,3 @@ def signup(request):
         template_data['form'] = UserCreationForm()
         return render(request, 'accounts/signup.html',
             {'template_data': template_data})
-    elif request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home.index')
-        else:
-            template_data['form'] = form
-            return render(request, 'accounts/signup.html',
-                          {'template_data': template_data})
-
-# def login(request):
-#     template_data = {}
-#     template_data['title'] = 'Login'
-#     if request.method == 'GET':
-#         return render(request, 'accounts/login.html', {'template_data': template_data})
-#     elif request.method == 'POST':
-#         user = authenticate(request, username = request.POST['username'], password = request.POST['password'])
-#         if user is None:
-#             template_data['error'] = 'The username or password is incorrect.'
-#             return render(request, 'accounts/login.html', {'template_data': template_data})
-#         else:
-#             auth_login(request, user)
-#             return redirect('home.index')
