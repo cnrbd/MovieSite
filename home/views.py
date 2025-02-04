cf7df8bfe6d8151849208d986ddcd04a49a5f6d8
Wei Chen Liu <Wliu0571@gmail.com>
2025-02-04 17:51:05 -0500

    testing new laptop

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 #render method takes in:
 #1) the HTTP request to be process
@@ -6,6 +7,8 @@ from django.shortcuts import render
 
 #store app templates under the next directory structure – app_name/templates/app_name/my_template.html.
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
 def index(request):
     return render(request, 'home/index.html')
 
--- movie_store/urls.py
+++ movie_store/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('home.urls')),
+    path("", include('home.urls')), #home page of the project
     # all urls from the home.urls files is included in this file because of the line above.
     path('movies/', include('movies.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
