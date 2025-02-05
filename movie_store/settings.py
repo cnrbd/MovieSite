33c7d32d968b469c6af206d6d6fd8b763933fc7c
cnrbd <puong23@ssis.edu.vn>
2025-02-05 17:15:29 -0500

    movies page

--- movie_store/settings.py
+++ movie_store/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_ROOT = os.path.join(BASE_DIR)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 TAILWIND_APP_NAME = 'theme'
--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'movies.index')
+    path('', views.index, name = 'movies.index'),
+    path('show/<int:id>/', views.show, name='movies.show')
 ]
