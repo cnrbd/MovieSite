8312c8b5c3eb5bd16f08bc0913eb5bf6fc984f25
cnrbd <puong23@ssis.edu.vn>
2025-02-10 17:52:51 -0500

    fix a FAT BUG

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,7 @@ def login(request):
     if request.method == 'GET':
         return render(request, 'accounts/login.html', {'template_data': template_data})
     elif request.method == 'POST':
-        user = authenticate(request, email = request.POST['email'], password = request.POST['password'])
+        user = authenticate(request, username = request.POST['username'], password = request.POST['password'])
         if user is None:
             template_data['error'] = 'The username or password is incorrect.'
             return render(request, 'accounts/login.html', {'template_data': template_data})
