6154008734a191bfce548f06efc4312c4cf6a2cc
cnrbd <puong23@ssis.edu.vn>
2025-02-10 17:33:33 -0500

    fix bug login

--- accounts/views.py
+++ accounts/views.py
@@ -10,10 +10,10 @@ def login(request):
     if request.method == 'GET':
         return render(request, 'accounts/login.html', {'template_data': template_data})
     elif request.method == 'POST':
-        user = authenticate(request, username = request.POST['username'], password = request.POST['password'])
+        user = authenticate(request, email = request.POST['email'], password = request.POST['password'])
         if user is None:
             template_data['error'] = 'The username or password is incorrect.'
-            return render(request, 'login.html', {'template_data': template_data})
+            return render(request, 'accounts/login.html', {'template_data': template_data})
         else:
             auth_login(request, user)
             return redirect('home:index')
