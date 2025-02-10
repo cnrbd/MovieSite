215478671aa359470823497dab983c85158375fc
nayavw <nwooldridge3@gatech.edu>
2025-02-10 15:52:45 -0500

    Login page design

--- accounts/views.py
+++ accounts/views.py
@@ -8,12 +8,12 @@ def login(request):
     template_data = {}
     template_data['title'] = 'Login'
     if request.method == 'GET':
-        return render(request, 'accounts/login.html', {'template_data': template_data})
+        return render(request, 'login.html', {'template_data': template_data})
     elif request.method == 'POST':
         user = authenticate(request, username = request.POST['username'], password = request.POST['password'])
         if user is None:
             template_data['error'] = 'The username or password is incorrect.'
-            return render(request, 'accounts/login.html', {'template_data': template_data})
+            return render(request, 'login.html', {'template_data': template_data})
         else:
             auth_login(request, user)
             return redirect('home:index')
