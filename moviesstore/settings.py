3516bd8a1072ff9a09ca699fa680b507b6edd0b6
Wei Chen Liu <Wliu0571@gmail.com>
2025-02-16 18:21:21 -0500

    settings to host

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -126,6 +126,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'accounts', 'static'),
 ]
